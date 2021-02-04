016e63925143f58aa20372aca53ad38b520114cb
mfilipav <mfilipav@ethz.ch>
2021-02-04 19:10:22 +0100

    wip: built initial prototype for ContinuousEmbeddings class

--- joeynmt/embeddings.py
+++ joeynmt/embeddings.py
@@ -53,3 +53,54 @@ class Embeddings(nn.Module):
     def __repr__(self):
         return "%s(embedding_dim=%d, vocab_size=%d)" % (
             self.__class__.__name__, self.embedding_dim, self.vocab_size)
+
+
+class ContinuousEmbeddings(nn.Module):
+
+    """
+    Simple embeddings class for continuous features
+    """
+
+    # pylint: disable=unused-argument
+    def __init__(self,
+                 embedding_dim: int = 64,
+                 input_size: int,  # new, dim of cont feature vector
+                 scale: bool = False,
+                 vocab_size: int = 0,
+                #  padding_idx: int = 1,
+                 freeze: bool = False,
+                 **kwargs):
+        """
+        Create new embeddings for the vocabulary.
+        Use scaling for the Transformer.
+
+        :param embedding_dim:
+        :param input_size: dim of cont feature vector
+        :param scale:
+        :param vocab_size:
+        :param freeze: freeze the embeddings during training
+        """
+        super().__init__()
+
+        self.embedding_dim = embedding_dim
+        self.scale = scale
+        self.ln = nn.Linear(self.input_size, self.embedding_dim)
+
+        if freeze:
+            freeze_params(self)
+
+    # pylint: disable=arguments-differ
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Perform lookup for input `x` in the embedding table.
+
+        :param x: index in the vocabulary
+        :return: embedded representation for `x`
+        """
+        if self.scale:
+            return self.ln(x) * math.sqrt(self.embedding_dim)
+        return self.ln(x)
+
+    def __repr__(self):
+        return "%s(embedding_dim=%d, input_size=%d)" % (
+            self.__class__.__name__, self.embedding_dim, self.input_size)
